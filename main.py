63ede0f1a84b1eeaf7b297995ac0af3ef735823d
Vrishank <vrishanknarayana.v@gmail.com>
2024-07-23 16:02:07 +0100

    Day 27: Sent an email with the titles and descriptions

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 api_key = "2a399d1986aa4ea6844cd521d8bdd9e7"
 url = ("https://newsapi.org/v2/everything?q=tesla&from=2024-06-23"
@@ -11,6 +12,10 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and descriptions
+body = ""
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None
+        body = body + article["title"] + "\n" + article["description"] + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(body)
